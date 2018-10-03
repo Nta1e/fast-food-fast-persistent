f4bcdcf671dc48a7e663ae7e969e46e918525e63
Ntale <Shadikntale@gmail.com>
2018-10-03 18:27:39 +0300

    modify procfile

--- app/models/database.py
+++ app/models/database.py
@@ -12,7 +12,7 @@ class Database(object):
 
     def init_app(self, app):
         self.conn = psycopg2.connect(
-            "dbname='fastfoodfast'  user='grey' host='127.0.0.1' password='Grey'")
+            "dbname='d9qctcer3pq18m'  user='jbuzgihuevyrav' port='5432' host='ec2-23-23-80-20.compute-1.amazonaws.com' password='f19a69dd3f99061fc38411a8af5ff7c0a9450f546eccf890034d8f7c68410228'")
         self.cur = self.conn.cursor(cursor_factory=RealDictCursor)
 
     def query(self, query):
