31b60aae6ac46df85f2fc769ce427b9775134978
Ntale <Shadikntale@gmail.com>
2018-10-03 17:10:13 +0300

    modify run file

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+from flask import redirect
 from app import app
 from schema import main
 
+
+@app.route("/")
+def default():
+    return redirect('/apidocs')
 
 if __name__ == '__main__':
     main()
